4efed8c225e2070a3aac84aff9217c3f035f9422
uniqueimaginate <uniqueimaginate@gmail.com>
2018-08-21 16:31:31 +0900

    C_learning_23

---
+++ fileDivision&headerFile/structure/structure/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "stdiv2.h"
+#include "intdiv4.h"
+
+
+int main()
+{
+	Div val = IntDiv(5, 2);
+	printf("목 : %d \n", val.quotient);
+	printf("나머지 : %d \n", val.remainder);
+	return 0;
+}
---
+++ fileDivision&headerFile/structure/structure/stdiv2.h
@@ -0,0 +1,10 @@
+#ifndef __STDIV2_H__
+#define __STDIV2_H__
+
+typedef struct div
+{
+	int quotient;
+	int remainder;
+} Div;
+
+#endif
